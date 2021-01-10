cd80a1aa46c6e633bfb88df4ddb8a2b530bf6558
Guepardow <mehdi.miah@gmail.com>
2021-01-09 21:09:29 -0500

    Correct scripts for efficientnetB0

--- script/run_WildTrack.sh
+++ script/run_WildTrack.sh
@@ -1,6 +1,6 @@
 for scene in C1 C2 C3 C4 C5 C6 C7
 do 
-for feature in bw colors hog vgg19 resnet18 densenet121 efficientB0 osnetAINMarket
+for feature in bw colors hog vgg19 resnet18 densenet121 efficientnetB0 osnetAINMarket
 do
 for sigma in 0 5 10 20
 do 
